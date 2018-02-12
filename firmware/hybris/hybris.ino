b603c28a8b4ee49b75830c83f81737751dd4548a
chaffneue <chaffneue@gmail.com>
2018-02-12 01:44:50 -0800

    Add pulsating LED on pending connection

--- firmware/hybris/hybris.ino
+++ firmware/hybris/hybris.ino
@@ -6,6 +6,7 @@
 #include "bluetooth.h"
 #include "keypress.h"
 #include "encoder.h"
+#include <math.h>
 
 void setup(void) {
   Serial.begin(115200);
@@ -13,6 +14,8 @@ void setup(void) {
   //NRF_TWIS1 ->ENABLE = 0; //disable TWI Slave
   sd_power_mode_set(NRF_POWER_MODE_LOWPWR);
   sd_power_dcdc_mode_set(NRF_POWER_DCDC_DISABLE);
+  pinMode(27, OUTPUT);
+  analogWrite(27, 0);
   
   init_bluetooth();
   keypress_init();
@@ -29,5 +32,18 @@ void loop() {
   if (is_key_pressed()) {
     keyboard_task();
   }
+
+  if(!is_bluetooth_connected()) {
+    float val = 1 + (exp(sin(millis()/2000.0*PI)) - 0.36787944) * 107.0;
+
+    analogWrite(27, val);
+
+    delayMicroseconds(50);
+
+    if(is_bluetooth_connected()) {
+      analogWrite(27, 0);
+    }
+  }
+  
   __WFI();
 }
