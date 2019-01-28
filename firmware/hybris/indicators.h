95462190f34fb6780e0fd4c198bbd72b54ccc0df
Richard Hoar <chaffneue@gmail.com>
2019-01-27 16:46:01 -0800

    adding LED_ON constant

--- firmware/hybris/indicators.h
+++ firmware/hybris/indicators.h
@@ -8,6 +8,7 @@
 // @see http://sean.voisen.org/blog/2011/10/breathing-led-with-arduino/
 #define get_level() (1 + (exp(sin(millis()/2000.0*PI)) - 0.36787944) * 107.0)
 #define LED_OFF 0
+#define LED_ON 1
 
 /** Watcher tasks for USB charging events
 */
